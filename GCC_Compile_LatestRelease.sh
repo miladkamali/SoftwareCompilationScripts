ec914f3f8ac91b4c7c5290796ad76c46afe71737
MKZ <>
2019-11-15 16:28:06 +0330

    added checking steps for each level of compilation scripts

--- GCC_Compile_LatestRelease.sh
+++ GCC_Compile_LatestRelease.sh
@@ -1,34 +1,24 @@
 #/bin/bash
+. ./step.sh
+step "installing dependencies"
+{
+    InstallationLocation="/usr/local"
+    version="9.2.0"
+    sourceStoreLocation="~/software_used"
+    try apt install -y build-essential lynx wget checkinstall
+} >/dev/null 2>&1
+next
 
-#
-# This file is part of the publicSource  distribution (https://github.com/miladkamali/).
-# Copyright (c) 2019 Milad Zare Kamali.
-#
-# This program is free software: you can redistribute it and/or modify  
-# it under the terms of the GNU General Public License as published by  
-# the Free Software Foundation, version 3.
+step "getting latest version of released gcc and installed gcc"
+{
+latestAvailableVersion=`try lynx --dump "ftp://ftp.gnu.org/gnu/gcc/"|tr ' ' '\n'|grep "gcc-[0-9]"|tail -n1|cut -d '-' -f2`
+latestInstalledVersion=`try ls $InstallationLocation -rt|grep gcc-[0-9]|cut -d '-' -f2`
+} >/dev/null 2>&1
+next
 
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-# General Public License for more details.
-
-# You should have received a copy of the GNU General Public License 
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-
-# TODO: do every thing as normal user then for installation switch to super user
-# TODO: check if each step is completed properly before continuing with the next step
-InstallationLocation="/usr/local"
-version="9.2.0"
-sourceStoreLocation="~/software_used"
-apt install -y build-essential lynx wget
-latestAvailableVersion=`lynx --dump "ftp://ftp.gnu.org/gnu/gcc/"|tr ' ' '\n'|grep "gcc-[0-9]"|tail -n1|cut -d '-' -f2`
-latestInstalledVersion=`ls $InstallationLocation -rt|grep gcc-[0-9]|cut -d '-' -f2`
 if [ $latestAvailableVersion==latestInstalledVersion ]
 then
-    echo "yes";echo "$latestAvailableVersion==$latestInstalledVersion";
-    echo "Latest GCC installed is gcc-$latestInstalledVersion but latest available gcc is gcc-$latestInstalledVersion"
+    echo "Latest GCC installed is gcc-$latestInstalledVersion but latest available gcc is gcc-$latestAvailableVersion"
     echo "say yes to compile and install the latest version"
     read response
     if [ $response == "yes" ]
@@ -37,20 +27,31 @@ then
         then
             mkdir $sourceStoreLocation
         fi
-        cd $sourceStoreLocation
-        wget "ftp://ftp.gnu.org/gnu/gcc/gcc-$version/gcc-$version.tar.xz"
-        tar xf "gcc-$version.tar.xz"
-        cd "gcc-$version"
-        ./contrib/download_prerequisites
-        cd ..
-        mkdir build
-        cd build
-        ../gcc-$version/configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=$InstallationLocation/gcc-$version --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib --program-suffix=-$version
-        make -j 8
+	step "downloading gcc-$version"
+	{
+            cd $sourceStoreLocation
+            try wget "ftp://ftp.gnu.org/gnu/gcc/gcc-$version/gcc-$version.tar.xz"
+            try tar xf "gcc-$version.tar.xz"
+            try cd "gcc-$version"
+            try ./contrib/download_prerequisites
+	} >/dev/null 2>&1
+	next
 
-        make install-strip
-        echo "export export PATH=/usr/local/gcc-$version/bin:$PATH" >> ~/.bashrc
-        echo "export LD_LIBRARY_PATH=/usr/local/gcc-$version/lib64:$LD_LIBRARY_PATH" >>~/.bashrc
+	step "configuring and compiling gcc-$version"
+	{
+            try cd ..
+            try mkdir build
+            try cd build
+            try ../gcc-$version/configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=$InstallationLocation/gcc-$version --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib --program-suffix=-$version
+            try make -j 8
+	}  >/dev/null 2>&1
+	next
+	step "installing and creating deb package"
+	{
+            try checkinstall -D make install
+            try echo "export export PATH=/usr/local/gcc-$version/bin:$PATH" >> ~/.bashrc
+            try echo "export LD_LIBRARY_PATH=/usr/local/gcc-$version/lib64:$LD_LIBRARY_PATH" >>~/.bashrc
+	}
 
     fi
 
